80fb78e99ab55a5b5ce86fca69a91d040542e75e
Iname Sname <johndoe@example.com>
2025-09-20 21:20:07 +0400

    format config

--- aws-N-layer/main.tf
+++ aws-N-layer/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 resource "aws_vpc" "vpc-x-prod" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
@@ -24,10 +24,10 @@ resource "aws_internet_gateway" "x-prod-IGW" {
 
 # create 2 public Subnets
 resource "aws_subnet" "x-prod-net-pub-A" {
-  vpc_id = aws_vpc.vpc-x-prod.id
-  cidr_block = "10.0.11.0/24"
+  vpc_id                  = aws_vpc.vpc-x-prod.id
+  cidr_block              = "10.0.11.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "eu-central-1a"
+  availability_zone       = "eu-central-1a"
 
   tags = {
     Name = "x-prod-net-pub-A"
@@ -35,10 +35,10 @@ resource "aws_subnet" "x-prod-net-pub-A" {
 }
 
 resource "aws_subnet" "x-prod-net-pub-B" {
-  vpc_id = aws_vpc.vpc-x-prod.id
-  cidr_block = "10.0.21.0/24"
+  vpc_id                  = aws_vpc.vpc-x-prod.id
+  cidr_block              = "10.0.21.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "eu-central-1b"
+  availability_zone       = "eu-central-1b"
 
   tags = {
     Name = "x-prod-net-pub-B"
@@ -61,12 +61,12 @@ resource "aws_route_table" "x-prod-RT-pub" {
 
 # associate the Route Table with public Subnets
 resource "aws_route_table_association" "public_assoc_a" {
-  subnet_id = aws_subnet.x-prod-net-pub-A.id
+  subnet_id      = aws_subnet.x-prod-net-pub-A.id
   route_table_id = aws_route_table.x-prod-RT-pub.id
 }
 
 resource "aws_route_table_association" "public_assoc_b" {
-  subnet_id = aws_subnet.x-prod-net-pub-B.id
+  subnet_id      = aws_subnet.x-prod-net-pub-B.id
   route_table_id = aws_route_table.x-prod-RT-pub.id
 }
 
@@ -91,8 +91,8 @@ resource "aws_eip" "nat_eip_b" {
 
 resource "aws_nat_gateway" "x-prod-NATGW-net-A" {
   connectivity_type = "public"
-  allocation_id = aws_eip.nat_eip_a.id
-  subnet_id = aws_subnet.x-prod-net-pub-A.id
+  allocation_id     = aws_eip.nat_eip_a.id
+  subnet_id         = aws_subnet.x-prod-net-pub-A.id
 
   tags = {
     Name = "x-prod-NATGW-net-A"
@@ -101,8 +101,8 @@ resource "aws_nat_gateway" "x-prod-NATGW-net-A" {
 
 resource "aws_nat_gateway" "x-prod-NATGW-net-B" {
   connectivity_type = "public"
-  allocation_id = aws_eip.nat_eip_b.id
-  subnet_id = aws_subnet.x-prod-net-pub-B.id
+  allocation_id     = aws_eip.nat_eip_b.id
+  subnet_id         = aws_subnet.x-prod-net-pub-B.id
 
   tags = {
     Name = "x-prod-NATGW-net-B"
@@ -113,10 +113,10 @@ resource "aws_nat_gateway" "x-prod-NATGW-net-B" {
 
 # create 2 private Subnets
 resource "aws_subnet" "x-prod-net-priv-A" {
-  vpc_id = aws_vpc.vpc-x-prod.id
-  cidr_block = "10.0.12.0/24"
+  vpc_id                  = aws_vpc.vpc-x-prod.id
+  cidr_block              = "10.0.12.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "eu-central-1a"
+  availability_zone       = "eu-central-1a"
 
   tags = {
     Name = "x-prod-net-priv-A"
@@ -124,10 +124,10 @@ resource "aws_subnet" "x-prod-net-priv-A" {
 }
 
 resource "aws_subnet" "x-prod-net-priv-B" {
-  vpc_id = aws_vpc.vpc-x-prod.id
-  cidr_block = "10.0.22.0/24"
+  vpc_id                  = aws_vpc.vpc-x-prod.id
+  cidr_block              = "10.0.22.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "eu-central-1b"
+  availability_zone       = "eu-central-1b"
 
   tags = {
     Name = "x-prod-net-priv-B"
@@ -139,7 +139,7 @@ resource "aws_route_table" "x-prod-RT-priv-A" {
   vpc_id = aws_vpc.vpc-x-prod.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.x-prod-NATGW-net-A.id
   }
 
@@ -152,7 +152,7 @@ resource "aws_route_table" "x-prod-RT-priv-B" {
   vpc_id = aws_vpc.vpc-x-prod.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block     = "0.0.0.0/0"
     nat_gateway_id = aws_nat_gateway.x-prod-NATGW-net-B.id
   }
 
@@ -163,12 +163,12 @@ resource "aws_route_table" "x-prod-RT-priv-B" {
 
 # associate Route Tables with private Subnets
 resource "aws_route_table_association" "private_assoc_a" {
-  subnet_id = aws_subnet.x-prod-net-priv-A.id
+  subnet_id      = aws_subnet.x-prod-net-priv-A.id
   route_table_id = aws_route_table.x-prod-RT-priv-A.id
 }
 
 resource "aws_route_table_association" "private_assoc_b" {
-  subnet_id = aws_subnet.x-prod-net-priv-B.id
+  subnet_id      = aws_subnet.x-prod-net-priv-B.id
   route_table_id = aws_route_table.x-prod-RT-priv-B.id
 }
 
@@ -176,10 +176,10 @@ resource "aws_route_table_association" "private_assoc_b" {
 
 # create 2 database Subnets
 resource "aws_subnet" "x-prod-net-db-A" {
-  vpc_id = aws_vpc.vpc-x-prod.id
-  cidr_block = "10.0.13.0/24"
+  vpc_id                  = aws_vpc.vpc-x-prod.id
+  cidr_block              = "10.0.13.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "eu-central-1a"
+  availability_zone       = "eu-central-1a"
 
   tags = {
     Name = "x-prod-net-db-A"
@@ -187,10 +187,10 @@ resource "aws_subnet" "x-prod-net-db-A" {
 }
 
 resource "aws_subnet" "x-prod-net-db-B" {
-  vpc_id = aws_vpc.vpc-x-prod.id
-  cidr_block = "10.0.23.0/24"
+  vpc_id                  = aws_vpc.vpc-x-prod.id
+  cidr_block              = "10.0.23.0/24"
   map_public_ip_on_launch = false
-  availability_zone = "eu-central-1b"
+  availability_zone       = "eu-central-1b"
 
   tags = {
     Name = "x-prod-net-db-B"
@@ -208,12 +208,12 @@ resource "aws_route_table" "x-prod-RT-db" {
 
 # associate the Route Table with database Subnets
 resource "aws_route_table_association" "database_assoc_a" {
-  subnet_id = aws_subnet.x-prod-net-db-A.id
+  subnet_id      = aws_subnet.x-prod-net-db-A.id
   route_table_id = aws_route_table.x-prod-RT-db.id
 }
 
 resource "aws_route_table_association" "database_assoc_b" {
-  subnet_id = aws_subnet.x-prod-net-db-B.id
+  subnet_id      = aws_subnet.x-prod-net-db-B.id
   route_table_id = aws_route_table.x-prod-RT-db.id
 }
 
@@ -221,30 +221,30 @@ resource "aws_route_table_association" "database_assoc_b" {
 
 # create Security Group for Bastion host & allow SSH and ICMP traffic
 resource "aws_security_group" "x-prod-bastion-ssh-SG" {
-  name = "x-prod-bastion-ssh-SG"
+  name        = "x-prod-bastion-ssh-SG"
   description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id = aws_vpc.vpc-x-prod.id
+  vpc_id      = aws_vpc.vpc-x-prod.id
 
   ingress {
     description = "Allow all ICMP"
-    from_port = -1
-    to_port = -1
-    protocol = "icmp"
+    from_port   = -1
+    to_port     = -1
+    protocol    = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
     description = "Allow all SSH"
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -255,19 +255,19 @@ resource "aws_security_group" "x-prod-bastion-ssh-SG" {
 
 # key pair for Bastion
 resource "aws_key_pair" "x-prod-bastion-key" {
-  key_name = "x-prod-bastion-key"
+  key_name   = "x-prod-bastion-key"
   public_key = file("~/.ssh/my-key.pub")
 }
 
 # Launch Template - based on Amazon Linux 2023 kernel-6.1 AMI
 resource "aws_launch_template" "x-prod-bastion-LC" {
-  name = "x-prod-bastion-LC"
-  image_id = "ami-0a72753edf3e631b7"
+  name          = "x-prod-bastion-LC"
+  image_id      = "ami-0a72753edf3e631b7"
   instance_type = "t2.micro"
-  key_name = aws_key_pair.x-prod-bastion-key.key_name
+  key_name      = aws_key_pair.x-prod-bastion-key.key_name
 
   network_interfaces {
-    security_groups = [aws_security_group.x-prod-bastion-ssh-SG.id]
+    security_groups             = [aws_security_group.x-prod-bastion-ssh-SG.id]
     associate_public_ip_address = true
   }
 
@@ -282,14 +282,14 @@ resource "aws_launch_template" "x-prod-bastion-LC" {
 
 # ASG with 1 instance of Bastion
 resource "aws_autoscaling_group" "x-prod-bastion-ASG" {
-  name = "x-prod-bastion-ASG"
+  name                = "x-prod-bastion-ASG"
   vpc_zone_identifier = [aws_subnet.x-prod-net-pub-A.id, aws_subnet.x-prod-net-pub-B.id]
-  desired_capacity = 1
-  max_size = 1
-  min_size = 1
+  desired_capacity    = 1
+  max_size            = 1
+  min_size            = 1
 
   launch_template {
-    id = aws_launch_template.x-prod-bastion-LC.id
+    id      = aws_launch_template.x-prod-bastion-LC.id
     version = "$Latest"
   }
 }
@@ -298,11 +298,11 @@ resource "aws_autoscaling_group" "x-prod-bastion-ASG" {
 
 # create test EC2 instance in private-a Subnet (App Tier)
 resource "aws_instance" "EC2-priv-A" {
-  ami = "ami-0a72753edf3e631b7"
-  instance_type = "t2.micro"
-  key_name = "x-prod-bastion-key"
+  ami                    = "ami-0a72753edf3e631b7"
+  instance_type          = "t2.micro"
+  key_name               = "x-prod-bastion-key"
   vpc_security_group_ids = [aws_security_group.x-prod-bastion-ssh-SG.id]
-  subnet_id = aws_subnet.x-prod-net-priv-A.id
+  subnet_id              = aws_subnet.x-prod-net-priv-A.id
 
   tags = {
     Name = "EC2-priv-A"
@@ -311,11 +311,11 @@ resource "aws_instance" "EC2-priv-A" {
 
 # create test EC2 instance in db-b Subnet (DB Tier)
 resource "aws_instance" "EC2-db-B" {
-  ami = "ami-0a72753edf3e631b7"
-  instance_type = "t2.micro"
-  key_name = "x-prod-bastion-key"
+  ami                    = "ami-0a72753edf3e631b7"
+  instance_type          = "t2.micro"
+  key_name               = "x-prod-bastion-key"
   vpc_security_group_ids = [aws_security_group.x-prod-bastion-ssh-SG.id]
-  subnet_id = aws_subnet.x-prod-net-db-B.id
+  subnet_id              = aws_subnet.x-prod-net-db-B.id
 
   tags = {
     Name = "EC2-db-B"
